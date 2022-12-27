cf27e75b682d1d24300de5fc1e2efe9b2a79a716
ChiendeGlace <szymon.szostak72@gmail.com>
2022-12-27 18:06:03 +0100

    Add styling and background images

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const humidity = document.querySelector('#humidity');
 const windSpeed = document.querySelector('#windspeed');
 const pressure = document.querySelector('#pressure');
 const converter = document.querySelector('#converter');
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
 
 const API_KEY = 'ee1d8e5931d2ed22eaeb20ec46591503';
 const CURRENT_DATE = new Date();
@@ -41,14 +41,36 @@ const displayData = (
     weatherTypeIcon.className = '';
     if (weatherType == 'Clouds') {
         weatherTypeIcon.className = 'fa-solid fa-cloud';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/53594/blue-clouds-day-fluffy-53594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
     } else if (weatherType == 'Rain') {
         weatherTypeIcon.className = 'fa-solid fa-cloud-rain';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/1028600/pexels-photo-1028600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
     } else if (weatherType == 'Clear') {
         weatherTypeIcon.className = 'fa-solid fa-cloud-sun';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/1133505/pexels-photo-1133505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
     } else if (weatherType == 'Fog') {
         weatherTypeIcon.className = 'fa-solid fa-cloud-moon';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/45222/forest-fog-nature-winter-45222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
     } else if (weatherType == 'Haze') {
         weatherTypeIcon.className = 'fa-solid fa-smog';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/1065925/pexels-photo-1065925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
+    } else if (weatherType == 'Mist') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud-moon';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/45222/forest-fog-nature-winter-45222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
+    } else if (weatherType == 'Smoke') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud-moon';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/45222/forest-fog-nature-winter-45222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)';
+    } else if (weatherType == 'Snow') {
+        weatherTypeIcon.className = 'fa-solid fa-snowflake';
+        container.style.backgroundImage =
+            'url(https://images.pexels.com/photos/1571442/pexels-photo-1571442.jpeg?auto=compress&cs=tinysrgb&w=1600)';
     }
     cityName.textContent = cityDataName;
     cityTemp.textContent = Math.round(cityDataTemp - 273.15) + ' °C';
