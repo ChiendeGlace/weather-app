7e50459cac928a4b2b4a6e63e6c4c67daa0c4f83
ChiendeGlace <szymon.szostak72@gmail.com>
2022-12-27 15:16:34 +0100

    Add design, output

--- index.js
+++ index.js
@@ -1,34 +1,81 @@
-const response = document.querySelector('#response');
 const weatherForm = document.querySelector('#weather-form');
-const weatherInput = document.querySelector('#city-name');
+const weatherInput = document.querySelector('#city-input');
+const weatherTypeInfo = document.querySelector('#weather');
+const weatherTypeIcon = document.querySelector('#weather-icon');
+const cityName = document.querySelector('#city-name');
+const cityTemp = document.querySelector('#city-temp');
+const currentDate = document.querySelector('#date');
+const feelsLike = document.querySelector('#feels');
+const humidity = document.querySelector('#humidity');
+const windSpeed = document.querySelector('#windspeed');
+const pressure = document.querySelector('#pressure');
+const converter = document.querySelector('#converter');
+const body = document.querySelector('body');
+
+const API_KEY = 'ee1d8e5931d2ed22eaeb20ec46591503';
+const CURRENT_DATE = new Date();
+const CURRENT_HOUR =
+    CURRENT_DATE.toString().split(' ')[4].split(':')[0] +
+    ':' +
+    CURRENT_DATE.toString().split(' ')[4].split(':')[1];
 
 const fetchLocationWeather = (e) => {
     e.preventDefault();
     if (weatherInput.value !== '') {
-        const weatherFetchedData = fetchWeather(weatherInput.value);
+        fetchWeather(weatherInput.value);
     } else {
-        response.textContent = 'Wrong information or no information.';
+        alert('Wrong information or no information.');
     }
 };
 
-weatherForm.addEventListener('submit', fetchLocationWeather);
+const displayData = (
+    weatherType,
+    cityDataName,
+    cityDataTemp,
+    cityFeels,
+    cityHumidity,
+    cityWindSpeed,
+    cityPressure
+) => {
+    weatherTypeInfo.textContent = weatherType;
+    weatherTypeIcon.className = '';
+    if (weatherType == 'Clouds') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud';
+    } else if (weatherType == 'Rain') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud-rain';
+    } else if (weatherType == 'Clear') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud-sun';
+    } else if (weatherType == 'Fog') {
+        weatherTypeIcon.className = 'fa-solid fa-cloud-moon';
+    } else if (weatherType == 'Haze') {
+        weatherTypeIcon.className = 'fa-solid fa-smog';
+    }
+    cityName.textContent = cityDataName;
+    cityTemp.textContent = Math.round(cityDataTemp - 273.15) + ' °C';
+    currentDate.textContent = `${CURRENT_DATE.toDateString()} ${CURRENT_HOUR}`;
+    feelsLike.textContent =
+        'Feels like: ' + Math.round(cityFeels - 273.15) + ' °C';
+    humidity.textContent = 'Humidity: ' + cityHumidity + ' %';
+    windSpeed.textContent =
+        'Wind speed: ' + Math.round((cityWindSpeed * 18) / 5) + ' km/h';
+    pressure.textContent = 'Pressure: ' + cityPressure + ' hPa';
+};
 
-const API_KEY = 'ee1d8e5931d2ed22eaeb20ec46591503';
-const CURRENT_DATE = new Date();
 const fetchWeather = async (city) => {
     const result = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`
     );
     const weatherData = await result.json();
-    const cityName = weatherData.name;
-    const cityTemp = weatherData.main.temp;
-    const cityFeelsLike = weatherData.main.feels_like;
-    const cityWindSpeed = weatherData.wind.speed;
-    const cityHumidity = weatherData.main.humidity;
-    const cityPressure = weatherData.main.pressure;
-    response.textContent = `In ${cityName} the temp is ${
-        (cityTemp - 273, 15)
-    }℃, temp feels like ${(cityFeelsLike - 273, 15)}℃, the wind speed is ${
-        Math.round(cityWindSpeed * 18) / 5
-    }km/h, the humidity is ${cityHumidity}% and the pressure is ${cityPressure}hPa`;
+    displayData(
+        weatherData.weather[0].main,
+        weatherData.name,
+        weatherData.main.temp,
+        weatherData.main.feels_like,
+        weatherData.main.humidity,
+        weatherData.wind.speed,
+        weatherData.main.pressure
+    );
 };
+
+weatherForm.addEventListener('submit', fetchLocationWeather);
+window.addEventListener('load', fetchWeather('Warsaw'));
